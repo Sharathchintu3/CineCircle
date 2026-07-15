bacb8e135e7a93d12af82ffab888c40d86e12d3e
Sharath <sharathchintu2108@gmail.com>
2026-07-15 12:31:10 +0530

    Implemented review like and unlike APIs

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from reviews.views.api_views import ReviewCreateAPIView, ReviewUpdateAPIView, ReviewDeleteAPIView
+from reviews.views.api_views import ReviewCreateAPIView, ReviewUpdateAPIView, ReviewDeleteAPIView, ReviewLikeAPIView
 
 urlpatterns = [
     path("", ReviewCreateAPIView.as_view(), name="review-create"),
     path("<int:pk>/", ReviewUpdateAPIView.as_view(), name="review-update"),
     path("<int:pk>/delete/", ReviewDeleteAPIView.as_view(), name="review-delete"),
+    path("<int:pk>/like/", ReviewLikeAPIView.as_view(), name="review-like"),
 ]
--- reviews/views/api_views.py
+++ reviews/views/api_views.py
@@ -3,6 +3,9 @@ from rest_framework.permissions import IsAuthenticated
 from reviews.models import Review
 from reviews.serializers import ReviewSerializer
 from reviews.permissions import IsReviewOwner
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
 class ReviewCreateAPIView(generics.CreateAPIView):
     serializer_class = ReviewSerializer
@@ -21,3 +24,20 @@ class ReviewDeleteAPIView(generics.DestroyAPIView):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
     permission_classes = [IsAuthenticated, IsReviewOwner]
+
+class ReviewLikeAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, pk):
+        review = generics.get_object_or_404(Review, pk = pk)
+        if review.likes.filter(id=request.user.id).exists():
+            return Response(
+                {"message":"You already liked this review."},
+                status = status.HTTP_400_BAD_REQUEST,
+            )
+        review.likes.add(request.user)
+
+        return Response(
+            {"message":"Review liked successfully."},
+            status = status.HTTP_200_OK,
+        )
